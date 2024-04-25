118e086894a1214afd68a8dfadb4c4bc3bd341e7
leviAck <2481538544@qq.com>
2024-04-25 20:11:01 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -1,63 +1,42 @@
-// quiz3.rs
+// threads1.rs
 //
-// This quiz tests:
-// - Generics
-// - Traits
+// This program spawns multiple threads that each run for at least 250ms, and
+// each thread returns how much time they took to complete. The program should
+// wait until all the spawned threads have finished and should collect their
+// return values into a vector.
 //
-// An imaginary magical school has a new report card generation system written
-// in Rust! Currently the system only supports creating report cards where the
-// student's grade is represented numerically (e.g. 1.0 -> 5.5). However, the
-// school also issues alphabetical grades (A+ -> F-) and needs to be able to
-// print both types of report card!
+// Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
+// hint.
+
 //
-// Make the necessary code changes in the struct ReportCard and the impl block
-// to support alphabetical report cards. Change the Grade in the second test to
-// "A+" to show that your changes allow alphabetical grades.
-//
-// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+use std::thread;
+use std::time::{Duration, Instant};
 
-
-pub struct ReportCard<T> {
-    pub grade: T,
-    pub student_name: String,
-    pub student_age: u8,
-}
-
-impl<T:std::fmt::Display> ReportCard<T> {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn generate_numeric_report_card() {
-        let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Tom Wriggle".to_string(),
-            student_age: 12,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Tom Wriggle (12) - achieved a grade of 2.1"
-        );
+fn main() {
+    let mut handles = vec![];
+    for i in 0..10 {
+        handles.push(thread::spawn(move || {
+            let start = Instant::now();
+            thread::sleep(Duration::from_millis(250));
+            println!("thread {} is complete", i);
+            start.elapsed().as_millis()
+        }));
     }
 
-    #[test]
-    fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: "A+",
-            student_name: "Gary Plotter".to_string(),
-            student_age: 11,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
-        );
+    let mut results: Vec<u128> = vec![];
+    for handle in handles {
+        match handle.join() {
+            Ok(result) => results.push(result),
+            Err(_) => println!("A thread panicked"),
+        }
+    }
+
+    if results.len() != 10 {
+        panic!("Oh no! All the spawned threads did not finish!");
+    }
+
+    println!();
+    for (i, result) in results.into_iter().enumerate() {
+        println!("thread {} took {}ms", i, result);
     }
 }
--- exercises/tests/tests4.rs
+++ exercises/tests/tests4.rs
@@ -5,6 +5,7 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
+//
 
 struct Rectangle {
     width: i32,
@@ -12,7 +13,6 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    // Only change the test functions themselves
     pub fn new(width: i32, height: i32) -> Self {
         if width <= 0 || height <= 0 {
             panic!("Rectangle width and height cannot be negative!")
@@ -27,23 +27,20 @@ mod tests {
 
     #[test]
     fn correct_width_and_height() {
-        // This test should check if the rectangle is the size that we pass into its constructor
         let rect = Rectangle::new(10, 20);
         assert_eq!(rect.width, 10); // check width
         assert_eq!(rect.height, 20); // check height
     }
 
     #[test]
-    #[should_panic] // rust 内置的 会判断是是否会出现panic
+    #[should_panic(expected = "Rectangle width and height cannot be negative!")]
     fn negative_width() {
-        // This test should check if program panics when we try to create rectangle with negative width
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Rectangle width and height cannot be negative!")]
     fn negative_height() {
-        // This test should check if program panics when we try to create rectangle with negative height
         let _rect = Rectangle::new(10, -10);
     }
 }
